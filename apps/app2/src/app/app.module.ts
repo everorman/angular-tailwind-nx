cef3d388ad350bdc3f799d0b7a59856b915f43c2
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-06-19 15:08:03 -0500

    Creating new app

--- apps/app2/src/app/app.module.ts
+++ apps/app2/src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Lib1Module } from '@angular-nx-tailwind/lib1';
 import { Lib2Module } from '@angular-nx-tailwind/lib2';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    Lib1Module,
-    Lib2Module,
-  ],
+  imports: [BrowserModule, Lib1Module, Lib2Module],
   providers: [],
   bootstrap: [AppComponent],
 })
