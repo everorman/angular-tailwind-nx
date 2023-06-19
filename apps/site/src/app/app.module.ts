eb3b24b0e61a7a1ac0cf5931d42f81fb25ee645f
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-06-19 13:10:13 -0500

    Initial commit

--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { Lib1Module } from '@angular-nx-tailwind/lib1';
+import { Lib2Module } from '@angular-nx-tailwind/lib2';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    Lib1Module, 
+    Lib2Module
   ],
   providers: [],
   bootstrap: [AppComponent],
