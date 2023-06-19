0faec98ba06a46c6683c44c15f4d04f8eb56ed7b
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-06-19 13:27:56 -0500

    Creating lib for tailwind.config

--- libs/lib2/tailwind.config.js
+++ libs/lib2/tailwind.config.js
@@ -1,6 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
-const sharedTailwindConfig = require('../../tailwind.config');
+const sharedTailwindConfig = require('../tailwind-preset/tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,27 +9,5 @@ module.exports = {
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    colors: {
-      primary: {
-        light: '#5eead4',
-        DEFAULT: '#14b8a6',
-        dark: '#0f766e',
-      },
-      secondary: {
-        light: '#bae6fd',
-        DEFAULT: '#0ea5e9',
-        dark: '#0369a1',
-      },
-      white: '#ffffff',
-      black: '#000000',
-    },
-    spacing: {
-      sm: '0.5rem',
-      md: '1rem',
-      lg: '1.5rem',
-      xl: '2rem',
-    },
-  },
   plugins: [],
 };
